5c5e4ccd97657a59ef83ec1db134380d7588f66a
VikasGarg-Dir <vikas.dcrust@gmail.com>
2025-04-22 17:50:22 +0530

    Chatbot codes in Tabs folder added

---
+++ Tabs/chatbot_1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from chatbot_using_ollama import chatbot_ui  # Import chatbot UI function
+
+def app():
+    # st.title("A safe space to talk!😄")
+    chatbot_ui()  # Call chatbot function
